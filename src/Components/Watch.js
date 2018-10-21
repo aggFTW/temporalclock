55042114fbb40a9313753409495ce6f5b1bd9d26
Alejandro Guerrero <aggftw@gmail.com>
2018-10-21 00:27:00 -0700

    added slp and bootstrap

--- src/Components/Watch.js
+++ src/Components/Watch.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SunCalc from 'suncalc'
 import 'react-vis/dist/style.css';
 import {XYPlot, ArcSeries, MarkSeries, LabelSeries} from 'react-vis';
-// import {ToggleButton, ToggleButtonGroup} from 'react-bootstrap';
+import {ToggleButton, ToggleButtonGroup} from 'react-bootstrap';
 import moment from 'moment/moment.js';
 import timezone from 'moment-timezone';
 
@@ -10,8 +10,11 @@ const daySegments = 6;
 const nightSegments = daySegments;
 const degreesPerSecondInDay = 360 / (3600 * 24);
 const radiusText = 0.9;
+
+// See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 const cities = {
   "Seattle": {latitude: 47.6062, longitude: -122.3321, tz: "America/Los_Angeles", timezoneOffset: -7},
+  "SLP": {latitude: 22.1566, longitude: -100.9855, tz: "America/Mexico_City", timezoneOffset: -5},
   "Melbourne": {latitude: -37.8136, longitude: 144.9631, tz: "Australia/Melbourne", timezoneOffset: +11},
   "Reykjavik": {latitude: 64.1466, longitude: -21.9426, tz: "Atlantic/Reykjavik", timezoneOffset: +0}
 }
@@ -19,9 +22,6 @@ const cities = {
 export class Watch extends React.Component {
   constructor(props) {
     super(props);
-
-    this.handleCityChange = this.handleCityChange.bind(this);
-    this.handleOptionChange = this.handleOptionChange.bind(this);
 
     this.numberMarks = [...new Array(24)].map((row, index) => {
       let angle = -90 + (index * 360 / 24);
@@ -75,7 +75,6 @@ export class Watch extends React.Component {
   angleDifferenceBetweenTimes(d1, d2) {
     // d2 happens at same time or later than d1
     let timeDiffSeconds = (d2 - d1) / 1000;
-    console.error(timeDiffSeconds);
 
     return timeDiffSeconds * degreesPerSecondInDay;
   }
@@ -156,24 +155,14 @@ export class Watch extends React.Component {
     };
   }
 
-  handleCityChange(e) {
-    this.setState({ city: e });
-  }
-
-  handleOptionChange(changeEvent) {
-    this.setState({
-      city: changeEvent.target.value
-    });
-  }
-
   render() {
     if (!this.state.today) {
       return "updating";
     }
     
     let toggles = Object.keys(cities).map((city) => {
-      //return <ToggleButton key={city} value={city}>{city}</ToggleButton>
-      return <label><input key={city} value={city} type="radio" name="cities" onChange={this.handleOptionChange} checked={this.state.city === city}/>{city}<br/></label>
+      return <ToggleButton key={city} value={city}>{city}</ToggleButton>
+      // return <label><input key={city} value={city} type="radio" name="cities" onChange={this.handleOptionChange} checked={this.state.city === city}/>{city}<br/></label>
     });
 
     let temporalData = this.updateData();
@@ -203,17 +192,22 @@ export class Watch extends React.Component {
         Sunset: {this.state.sunset.toLocaleString()}<br />
         <br />
         <br />
-        {/* <ToggleButtonGroup
+        <ToggleButtonGroup
           type="radio"
           name="cities"
           value={this.state.city}
-          onChange={this.handleChange}
+          onChange={
+            (value) => {
+            this.setState({
+              city: value
+            });
+          }}
         >
           {toggles}
-        </ToggleButtonGroup> */}
-        <form>
+        </ToggleButtonGroup>
+        {/* <form>
           {toggles}
-        </form>
+        </form> */}
         <br />
         <br />
         <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
